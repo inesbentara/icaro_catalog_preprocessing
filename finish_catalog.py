a5481df5df8c641375344f6cb92aee1d1ccb72f9
Ines Bentara <ines.bentara@jupyter4.ligo.caltech.edu>
2025-07-03 01:28:34 -0700

    Remove Python scripts from root; all scripts now in scripts/ directory only

--- finish_catalog.py
+++
@@ -1,8 +0,0 @@
-import icarogw 
-outfolder = '/home/simone.mastrogiovanni/GLADEp_review/pixelated_gladep' 
-outfile = '/home/simone.mastrogiovanni/GLADEp_review/icaro_gladep.hdf5' 
-grouping = 'bJ-band' 
-subgrouping = 'eps-1' 
-icat = icarogw.catalog.icarogw_catalog(outfile,grouping,subgrouping)
-icat.build_from_pixelated_files(outfolder)
-icat.save_to_hdf5_file()
